60d6275bcd5fe3168ed2bed782811b969046a0eb
Vidwat Kanade <vidwatk@gmail.com>
2022-03-05 18:36:31 +0000

    tutorial(Stage 2): UseState counter and stuff

--- tutorialProject/App.js
+++ tutorialProject/App.js
@@ -6,20 +6,33 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
   View,
-  Button,
-  Linking
+  Button
 } from 'react-native';
 
 const App = () => { 
+const [name, setName] = useState("first")
+const[session, setSession] = useState({number: 6, title:"state"})
+const[current, setCurrent] = useState(true)
+const[counter, setCounter] = useState(0)
+
+const onClickHandler = () => {
+  setName("Second Update")
+  setSession({number: 7, title: "some random shit"})
+  setCurrent(false)
+  setCounter(counter + 1)
+}
   return (
     <View style= {styles.body}> 
-      <Text style={styles.text}>This is the first CLI app</Text>
-      <Button title="youtubeChannel" onPress={() => {Linking.openURL("https://github.com/vidwatk")}}></Button>
+      <Text style={styles.text}> {name} </Text>
+      <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
+      <Text style={styles.text}> {current ? "current Session" : "Next session"} </Text>
+      <Text style={styles.text}> This has been clicked {counter} </Text>
+      <Button title="Update State" onPress={onClickHandler}></Button>
     </View>
 
   );
@@ -34,7 +47,7 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#00008B",
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: "800",
     fontStyle: "italic",
     margin: 10 
